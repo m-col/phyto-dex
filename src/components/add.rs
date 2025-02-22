9057eea600c157c73dd7c1ece85f126b85a3d6d3
mcol <mcol@posteo.net>
2025-02-22 13:05:29 +0000

    enable more select box

--- src/components/add.rs
+++ src/components/add.rs
@@ -20,20 +20,20 @@ pub fn AddSpecimen() -> Element {
                 value: "{form_name}",
                 oninput: move |event| form_name.set(event.value())
             }
-            //select {
-            //    onchange: move |event| form_species.set(event.value().parse::<i32>().unwrap()),
-            //    option {
-            //        value: 0,
-            //        "Select a species..."
-            //    }
-            //    for (key, name) in available_species().unwrap() {
-            //        option {
-            //            key: key,
-            //            value: key,
-            //            "{name}"
-            //        }
-            //    }
-            //}
+            select {
+                //onchange: move |event| form_species.set(event.value().parse::<i32>().unwrap()),
+                option {
+                    value: 0,
+                    "Select a species..."
+                }
+                //for (key, name) in available_species().unwrap() {
+                //    option {
+                //        key: key,
+                //        value: key,
+                //        "{name}"
+                //    }
+                //}
+            }
             button {
                 onclick: move |_| async move {
                     warn!("Add specimen");
